c55fd506624f8a47488ebf0d37d84b28491d5da8
dir-FY24小林廉太郎 <rentarou.kobayashi.j411@dirbato.co.jp>
2024-04-26 11:47:16 +0900

    Lesson21終了

--- TutorialDevelop/src/main/java/com/techacademy/service/UserService.java
+++ TutorialDevelop/src/main/java/com/techacademy/service/UserService.java
@@ -1,6 +1,7 @@
 package com.techacademy.service;
 
 import java.util.List;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -20,10 +21,22 @@ public class UserService {
         return userRepository.findAll();
     }
     
+    public User getUser(Integer id) {
+        return userRepository.findById(id).get();
+    }
+    
     /**Userの登録を行う*/
     @Transactional
     public User saveUser(User user) {
         return userRepository.save(user);
+    }
+    
+    /**Userの削除を行う*/
+    @Transactional
+    public void deleteUser(Set<Integer> idck) {
+        for(Integer id : idck) {
+            userRepository.deleteById(id);
+        }
     }
 
 }
